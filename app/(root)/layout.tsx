5261f149b499b6779f2b60dd9c79df715c157d4a
NirajMalokar21 <nirajmalokar@gmail.com>
2024-08-07 12:54:24 +0530

    Create home page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: {children: React.ReactNode}) => {
+    return (
+      <main className='background-light850_dark100 relative'>
+        {children}
+      </main>
+    )
+  }
+  
+  export default Layout
