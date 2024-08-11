b9da1018d4602b1a0318fcaf728f35cc2aaa15a0
NirajMalokar21 <nirajmalokar@gmail.com>
2024-08-12 02:47:46 +0530

    Made Intro Component Responsive

--- components/homeSections/Intro.tsx
+++ components/homeSections/Intro.tsx
@@ -3,24 +3,19 @@ import GlobalSearch from '../shared/search/GlobalSearch'
 
 const Intro = () => {
   return (
-    <section className="pt-28 flex flex-col gap-12 justify-start items-start w-full ml-16 mr-24">
-        <div className="flex flex-col justify-center items-center gap-4 text-dark100_light900  pt-12">
-            <h1 className="h1-bold text-5xl">
-                Welcome to Your Ultimate Cooking Companion!
-            </h1>
-            <h2 className="h2-bold text-3xl pt-2">
-                Everything You Need, All in One Place.
-            </h2>
+    <section className="pt-28 flex flex-col max-sm:gap-6 md:gap-12 justify-center items-center w-full 
+    lg:ml-16 lg:mr-24 md:ml-6 md:mr-6 max-sm:ml-3 max-sm:mr-3">
+        <div className="flex flex-col justify-center text-center items-center max-sm:gap-2 md:gap-4 text-dark100_light900  
+        max-sm:pt-6 md:pt-12">
+            <div className='flex flex-col justify-center items-center gap-4'>
+                <h1 className="h1-bold max-sm:text-3xl md:text-4xl lg:text-5xl">
+                    Welcome to Your Ultimate Cooking Companion!
+                </h1>
+                <h2 className="h2-bold max-sm:text-xl md:text-2xl lg:text-3xl pt-2">
+                    Everything You Need, All in One Place.
+                </h2>
+            </div>
             <GlobalSearch />
-            <p className="lg:px-52 pt-8">
-                Discover a world of culinary creativity with our all-in-one cooking companion. Whether 
-                you&apos;re planning your meals with our smart shopping list, getting personalized recipe 
-                suggestions from our AI, or sharing your favorite dishes with a vibrant community, we&apos;ve 
-                got everything you need to elevate your cooking experience.
-            </p>
-        </div>
-        <div className='flex flex-col justify-center items-center bg-primary-100 mt-12'>
-            Hot Recipes
         </div>
     </section>
   )
