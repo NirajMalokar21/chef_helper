f6b87bd8cfe5b00851451e19811a34ab814248f2
NirajMalokar21 <nirajmalokar.com>
2024-09-16 07:09:02 +0530

    updated input component

--- components/shop-listComps/AddIngredient.tsx
+++ components/shop-listComps/AddIngredient.tsx
@@ -4,18 +4,18 @@ import { Button } from '../ui/button'
 
 const AddIngredient = () => {
   return (
-    <div className="w-full max-w-[900px] flex flex-col gap-8 justify-start items-start">
+    <div className="w-full max-w-[1200px] flex flex-col gap-8 justify-start items-center">
         <div className='w-full flex flex-col md:flex-row justify-start items-start gap-4'>
             <div className='w-full flex flex-col gap-3 justify-start items-start'>
                 <p className='paragraph-regular px-2 text-dark100_light900'>Ingredient<span className='text-red-500'>*</span></p>
-                <Input type='string' placeholder='Ingredient Name' className='h-[50px] shadow-lg px-2' />
+                <Input type='string' placeholder='Ingredient Name'/>
             </div>
             <div className='w-full md:w-[250px] flex flex-col gap-3 justify-start items-start'>
                 <p className='paragraph-regular px-2 text-dark100_light900'>Quantity</p>
-                <Input type='string' placeholder='Eg:200gm or 20ml' className='h-[50px] shadow-lg px-2' />
+                <Input type='string' placeholder='Eg:200gm or 20ml'/>
             </div>
         </div>
-        <Button className='bg-primary-600 dark:bg-primary-600 dark:text-light-900 font-semibold h-[50px] text-md w-full max-w-[900px]'>Add</Button>
+        <Button className='bg-primary-600 dark:bg-primary-600 dark:text-light-900 font-semibold h-[50px] text-md w-full'>Add</Button>
     </div>
   )
 }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-full bg-gray-300 px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 text-dark100_light900",
+          "flex w-full rounded-md h-[60px] shadow-lg bg-gray-300 px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 text-dark100_light900",
           className
         )}
         ref={ref}
