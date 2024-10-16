7bd672d36641d537a38919ecc319bf7e90f8d2a1
Niraj Malokar <nirajmalokar@gmail.com>
2024-10-16 12:04:46 -0400

    minor bug fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -55,15 +55,15 @@ export async function POST(req: Request) {
   const eventType = evt.type;
   if(eventType === 'user.created') {
       const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
-    //   const mongoUser = await createUser({
-    //       clerkId: id,
-    //       name: `${first_name}${last_name ? ` ${last_name}`: ''}`,
-    //       email: email_addresses[0].email_address,
-    //       username: username!,
-    //       picture: image_url
-    //   })
-    //   console.log('user added')
-    //   return NextResponse.json({ message: 'OK', user: mongoUser})
+      const mongoUser = await createUser({
+          clerkId: id,
+          name: `${first_name}${last_name ? ` ${last_name}`: ''}`,
+          email: email_addresses[0].email_address,
+          username: username!,
+          picture: image_url
+      })
+      console.log('user added')
+      return NextResponse.json({ message: 'OK', user: mongoUser})
   }
   else if(eventType === 'user.updated') {
   const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
