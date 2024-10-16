e427966dab2154075c0d5a26c6688dea2de19a6d
Niraj Malokar <nirajmalokar@gmail.com>
2024-10-16 11:04:20 -0400

    Add user Schema and start webhook config

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,10 @@
 import Features from "@/components/homeSections/Features";
 import Intro from "@/components/homeSections/Intro";
 import TopRecipes from "@/components/homeSections/TopRecipes";
+import { connectToDatabse } from "@/lib/mongoose";
 
 export default async function Home () {
+    await connectToDatabse();
     return (
         <main className="flex flex-col justify-center items-center md:px-4">
             <Intro />
